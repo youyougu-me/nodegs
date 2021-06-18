3a1861af61e4aedb7dfc6a462b0ae63d15aca072
youyougu-me <945029149@qq.com>
2021-06-18 15:32:29 +0800

    feat:修改注册逻辑

--- handlers/registerHandle.js
+++ handlers/registerHandle.js
@@ -1,12 +1,28 @@
 let companyModel = require('../model/companyModel')
 let systemPersonModel = require('../model/systemPersonModel')
+let mongoose = require('mongoose')
 
 module.exports = async (request, response) => {
   let {companyId, companyName} = request.body
   // 数据库错误会在这里捕捉到,而不是try
-  // 查到了data会返回查到的数据,没查到data返回null,若数据库错误data返回undefined
-  // 只要数据库无错误,查没查到err都为null,数据库有错误则返回错误 err判断这次查询是否成功
-  companyModel.findOne({_id: companyId}, (err, data) => {
+  // 没查到data返回null;数据库错误data返回undefined
+  // 数据库无错误,查没查到err都为null;数据库有错误err有值
+  // 避免崩溃 try一下
+  let companyId2
+  try {
+    companyId2 = mongoose.Types.ObjectId(companyId)
+  } catch (e) {
+    response.status(500).send({
+      data: null,
+      meta: {
+        msg: "公司校验码不存在"
+      }
+    })
+    // return之后后面不会执行
+    return
+  }
+
+  companyModel.findOne({_id: companyId2}, (err, data) => {
       // 数据库无错误
       if (!err) {
         if (data === null) {
@@ -46,24 +62,23 @@ module.exports = async (request, response) => {
 // 定义向数据库插入数据的函数
 let testInsertUserData = (request, response) => {
   let {companyId, companyName, personName, email, password} = request.body
-  try {
-    systemPersonModel.findOne({email: email}, (err, data) => {
-      // console.log(err)
-      // console.log(data)
+  systemPersonModel.findOne({email: email}, (err, data) => {
+    // 数据库无错误
+    if (!err) {
       if (data === null) {
-        systemPersonModel.create({email, personName, companyId, companyName, password}, (errson, data) => {
-          if (!errson) {
-            response.status(200).send({
-              data: null,
-              meta: {
-                msg: "注册成功"
-              }
-            })
-          } else {
+        systemPersonModel.create({email, personName, companyId, companyName, password}, (err2, data2) => {
+          if (err2) {
             response.status(500).send({
               data: null,
               meta: {
                 msg: "注册失败"
+              }
+            })
+          } else {
+            response.status(200).send({
+              data: null,
+              meta: {
+                msg: "注册成功"
               }
             })
           }
@@ -72,19 +87,20 @@ let testInsertUserData = (request, response) => {
         response.status(500).send({
           data: null,
           meta: {
-            msg: "邮箱已存在"
+            msg: "此邮箱已经注册过"
           }
         })
       }
-    })
-  } catch (err) {
-    response.status(500).send({
-      data: null,
-      meta: {
-        msg: "后端服务错误"
-      }
-    })
-  }
+
+    } else {
+      response.status(500).send({
+        data: null,
+        meta: {
+          msg: "后端服务错误"
+        }
+      })
+    }
+  })
 }
 
 // 一般来说,后端返回200就一定成功,500就给消息提示
